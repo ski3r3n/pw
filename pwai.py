d0468aff951085eed4c5153eef933ccbeb1b9bd8
Kie Ren <kieren.siew@gmail.com>
2024-01-12 16:53:56 +0800

    tes

--- pwai.py
+++ pwai.py
@@ -11,8 +11,7 @@ completion = client.chat.completions.create(
     messages=[
         {"role": "system", "content": "You sumarise teacher's lectures for students to understand. You make sure to include important points."},
         {"role": "user", "content": """This is the story of how Feynman's lost lecture
-came to be lost, and how it came to be found
-again. In April 1992, as Caltech's archivist, I was
+came to be lost, and how it came to be found again. In April 1992, as Caltech's archivist, I was
 asked by Gerry Neugebauer, the chairman of the
 Division of Physics, Mathematics and Astronomy, to go through the files in Robert Leighton's
 office. Leighton was ill and had not used his
@@ -24,4 +23,4 @@ would dispose of the rest. """}
     ]
 )
 
-print(completion.choices[0].message)
+print(completion.choices[0].message.content)
