904fce6ab4f000a4656244d3c871158bd5c2a607
Kie Ren <kieren.siew@gmail.com>
2024-01-14 21:36:04 +0800

    epic i made it better also new recording woo

--- pwai.py
+++ pwai.py
@@ -6,13 +6,12 @@ load_dotenv()
 openaiapikey = os.getenv("OPENAIAPIKEY")
 
 c = OpenAI(api_key=openaiapikey)
-audio_file = open("Recording(4).m4a", "rb")
+audio_file = open("Recording(5).m4a", "rb")
 
 transcript = c.audio.transcriptions.create(
     model="whisper-1",
     file=audio_file,
     response_format="text",
-    prompt="answer in english"
 )
 
 print(f"\n\nChatGPT:\n{transcript}")
@@ -20,16 +19,19 @@ print(f"\n\nChatGPT:\n{transcript}")
 client = OpenAI(api_key=openaiapikey)
 
 # temprompt = input("Your prompt: ")
-lang = input('what language u answer in: ')
+lang = input('what language do you want the answer in: ')
+
 longprompt = '''
 
 '''
+style = input("styling (point form, short paragraph, long essay): ")
+
 audioprompt = str(transcript) + f"answer in {lang}"
 #
 completion = client.chat.completions.create(
     model="gpt-3.5-turbo",
     messages=[
-        {"role": "system", "content": "You always answer in english. You sumarise teacher's lectures for students to understand. You make sure to include important points. Summarise whatever prompt is given, in bullet point format, ranked from most important to least important. Answer in english."},
+        {"role": "system", "content": f"You sumarise teacher's lectures for students to understand. You make sure to include important points. Summarise whatever prompt is given, in {style} format, ranked from most important to least important."},
         {"role": "user", "content": audioprompt}
     ]
 )
