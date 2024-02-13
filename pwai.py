dab5ebc40cee5862262b1a05fe2428fa55c6bf2e
Kie Ren <kieren.siew@gmail.com>
2024-02-13 11:51:41 +0000

    improved the prompt and added one more voiceline

--- pwai.py
+++ pwai.py
@@ -6,7 +6,7 @@ load_dotenv()
 openaiapikey = os.getenv("OPENAIAPIKEY")
 
 c = OpenAI(api_key=openaiapikey)
-audio_file = open("Recording(5).m4a", "rb")
+audio_file = open("whatever.mp3", "rb")
 
 transcript = c.audio.transcriptions.create(
     model="whisper-1",
@@ -24,14 +24,14 @@ lang = input('what language do you want the answer in: ')
 longprompt = '''
 
 '''
-style = input("styling (point form, short paragraph, long essay): ")
+style = input("styling (point form, short paragraph, long essay etc.): ")
 
-audioprompt = str(transcript) + f"answer in {lang}"
+audioprompt = f"{str(transcript)} answer in {lang}"
 #
 completion = client.chat.completions.create(
     model="gpt-3.5-turbo",
     messages=[
-        {"role": "system", "content": f"You sumarise teacher's lectures for students to understand. You make sure to include important points. Summarise whatever prompt is given, in {style} format, ranked from most important to least important."},
+        {"role": "system", "content": f"You summarise teacher's lectures for students to understand. You make sure to include important points. Summarise whatever prompt is given, in {style} format, ranked from most important to least important, filtering out the less important things, jokes and filler words."},
         {"role": "user", "content": audioprompt}
     ]
 )
